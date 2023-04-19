9561821224050eda281b56777ee4303c5cf70317
diegoserra <diego.serra@gmail.com>
2023-04-18 21:53:20 -0300

    nuevo capicua modificado

--- capicua.py
+++ capicua.py
@@ -6,3 +6,39 @@ Se debe evaluar que lo ingresado, sea un número entero positivo; de lo contrari
 se debe enviar el mensaje “Número Inválido”, y solicitar el siguiente.
 El ingreso de números, termina cuando en lugar de un número positivo, el usuario ingresa 0.
 """
+BASE_NUMERICA = 10
+
+def es_igual(x, y):
+  return x == y
+
+def es_dintinto(x, y):
+  return not es_igual(x, y)
+
+def solicitar_numero():
+  mensaje = "Ingrese un valor entero positivo o cero para finalizar:"
+  valor = int(input(mensaje))
+
+  while (valor < 0):
+    print("Numero Invalido\n")
+    valor = int(input(mensaje))
+
+  return valor
+
+def es_capicua(num):
+  return False
+
+def procesar_serie():
+  numero = solicitar_numero()
+
+  while es_dintinto(numero, 0):
+    if es_capicua(numero):
+      print(f'El numero {numero} es capicua\n')
+    else:
+      print(f'El numero {numero} no es capicua\n')
+
+    numero = solicitar_numero()
+
+
+print('Debe ingresar una serie de numeros enteros positivos y se le indicará si son capicua o no\n\n')
+procesar_serie()
+print('Programa finalizado.')
