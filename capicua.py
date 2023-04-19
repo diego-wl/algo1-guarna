585fadfdb617fd121d841d6aa260b911b7fe1459
diegoserra <diego.serra@gmail.com>
2023-04-18 21:19:53 -0300

    se agrega enunciado

---
+++ capicua.py
@@ -0,0 +1,8 @@
+""" 
+Escribir un programa que solicite el ingreso de una serie de números.
+Por cada número ingresado se deberá informar si el mismo es ó no, un número
+capicúa.
+Se debe evaluar que lo ingresado, sea un número entero positivo; de lo contrario,
+se debe enviar el mensaje “Número Inválido”, y solicitar el siguiente.
+El ingreso de números, termina cuando en lugar de un número positivo, el usuario ingresa 0.
+"""
